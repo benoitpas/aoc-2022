ec54c53d5bd377f1dee8f9bfd546a24f5f7aad6d
benoitpas <bpasquereau@gmail.com>
2022-12-01 20:56:27 +0000

    Day 1 fixed warnings

--- src/Day1.hs
+++ src/Day1.hs
@@ -7,12 +7,14 @@ module Day1
 
 import qualified Data.List as L
 
+calories:: [String] -> [Integer]
 calories allLines =
-    let (ps,s) = foldr (\l (prevSums, sum) -> case l of
-                                            "" -> (sum:prevSums, 0)
-                                            l -> (prevSums, sum + read l)) ([],0) allLines
+    let (ps,s) =  foldr (\l (prevSums, acc) -> case l of
+                                                "" -> (acc:prevSums, 0)
+                                                n -> (prevSums, acc + read n)) ([],0) allLines
     in (s:ps)
 
+max3Calories :: [Integer] -> Integer
 max3Calories = sum . take 3 . reverse . L.sort
 
 run :: IO ()
