a510726fb18dee70ad351a9a7bacc6e09be3a5f2
benoitpas <bpasquereau@gmail.com>
2022-12-05 18:46:56 +0000

    Day 4 part 2

--- src/Day4.hs
+++ src/Day4.hs
@@ -1,28 +1,41 @@
 module Day4
     (
         contains,
+        overlap,
         run,
     ) where
 
 import qualified Data.List as L
 
+split :: Eq a => a -> [a] -> [[a]]
 split c s = case L.elemIndex c s of
                 Just i -> [take i s, drop (i + 1) s]
                 _ -> []
 
+getIndexes :: (Read a, Read b, Num a, Num b) => [Char] -> (a, b)
 getIndexes s = case split '-' s of
                 smin:smax:_ -> (read smin, read smax)
                 _ -> (0,0)
 
-contains :: String -> Bool
-contains l = case split ',' l of
+iterateIntervals :: (Integer -> Integer -> Integer -> Integer -> Bool) -> String -> Bool
+iterateIntervals cond l = case split ',' l of
                 l1:l2:_ -> let (min1, max1) = getIndexes l1 in
                            let (min2, max2) = getIndexes l2
-                           in (min1 <= min2 && max2 <= max1) || (min2 <= min1 && max1 <= max2)
+                           in cond min1 max1 min2 max2
                 _ -> False
+
+contains :: String -> Bool
+contains = iterateIntervals (\min1 max1 min2 max2 -> (min1 <= min2 && max2 <= max1) || (min2 <= min1 && max1 <= max2))
+
+overlap :: String -> Bool
+overlap = iterateIntervals (\min1 max1 min2 max2 ->  (min1 <= max2 && min2 <= max1))
+
+countIntervals :: (a -> Bool) -> [a] -> Int
+countIntervals cond = length . (filter id) . (map cond)
 
 run :: IO ()
 run = do
     content <- readFile "src/day4_input.txt"
     let l = (lines content)
-    print ("puzzle 1: " ++ (show ( length (filter id (map contains l)))))
+    print ("puzzle 1: " ++ (show (countIntervals contains l)))
+    print ("puzzle 1: " ++ (show (countIntervals overlap l)))
--- test/Day4Spec.hs
+++ test/Day4Spec.hs
@@ -5,14 +5,24 @@ import Day4
 
 ex1 :: [String]
 ex1 =  ["2-4,6-8",
+        "6-8,2-4",
         "2-3,4-5",
+        "4-5,2-3",
         "5-7,7-9",
+        "7-9,5-7",
         "2-8,3-7",
+        "3-7,2-8",
         "6-6,4-6",
-        "2-6,4-8"]
+        "4-6,6-6",
+        "2-6,4-8",
+        "4-8,2-6"]
 
 spec :: Spec
 spec = do
+  describe "contains" $ do
+    it "find if the intervals contains one or the other" $ do
+        (map contains ex1) `shouldBe` [False,False,False,False,False,False,True,True,True,True,False,False]
+
   describe "overlap" $ do
     it "find if the intervals overlap" $ do
-        (map contains ex1) `shouldBe` [False,False,False,True,True,False]
+        (map overlap ex1) `shouldBe` [False,False,False,False,True,True,True,True,True,True,True,True]
