20c08fb348103ec72638c4e4d328b0222bbc8477
benoitpas <bpasquereau@gmail.com>
2022-12-06 07:17:36 +0000

    Day 6 part 2

--- test/Day6Spec.hs
+++ test/Day6Spec.hs
@@ -3,25 +3,41 @@ module Day6Spec (spec) where
 import Test.Hspec
 import Day6
 
-import qualified Data.Map as M
-
 ex1 :: String
 ex1 = "bvwbjplbgvbhsrlpgdmjqwftvncz"
-ex2 = "nppdvjthqldpwncqszvftbrmjlhg" -- : first marker after character 6
-ex3 = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" -- : first marker after character 10
+
+ex2 :: String
+ex2 = "nppdvjthqldpwncqszvftbrmjlhg"
+
+ex3 :: String
+ex3 = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
+
+ex4 :: String
 ex4 = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
 
 spec :: Spec
 spec = do
   describe "findMarker" $ do
     it "finds the marker for example 1" $ do
-        (findMarker ex1) `shouldBe` 5
+        (findMarker 4 ex1) `shouldBe` 5
   describe "findMarker" $ do
     it "finds the marker for example 2" $ do
-        (findMarker ex2) `shouldBe` 6
+        (findMarker 4 ex2) `shouldBe` 6
   describe "findMarker" $ do
     it "finds the marker for example 3" $ do
-        (findMarker ex3) `shouldBe` 10
+        (findMarker 4 ex3) `shouldBe` 10
   describe "findMarker" $ do
     it "finds the marker for example 4" $ do
-        (findMarker ex4) `shouldBe` 11
+        (findMarker 4 ex4) `shouldBe` 11
+  describe "findMarker" $ do
+    it "finds the message for example 1" $ do
+        (findMarker 14 ex1) `shouldBe` 23
+  describe "findMarker" $ do
+    it "finds the message for example 2" $ do
+        (findMarker 14 ex2) `shouldBe` 23
+  describe "findMarker" $ do
+    it "finds the message for example 3" $ do
+        (findMarker 14 ex3) `shouldBe` 29
+  describe "findMarker" $ do
+    it "finds the message for example 4" $ do
+        (findMarker 14 ex4) `shouldBe` 26
