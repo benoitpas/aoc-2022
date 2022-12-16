8240ddfa475ea3ff8c596334ce58e20fd7d5fd27
benoitpas <bpasquereau@gmail.com>
2022-12-16 07:27:31 +0000

    Day9 part 2

--- src/Day9.hs
+++ src/Day9.hs
@@ -1,30 +1,37 @@
 module Day9
     (
+        movePoint,
         moveTail,
+        addpoints,
         tailPositions,
         run,
     ) where
 
 import qualified Data.Set as S
 
-moveTail (tx,ty) (hx,hy) = if abs (hx-tx) > 1 || abs (hy-ty) > 1 
+movePoint (tx,ty) (hx,hy) = if abs (hx-tx) > 1 || abs (hy-ty) > 1
                             then (tx + signum (hx-tx), ty + signum (hy-ty))
                             else (tx, ty)
 
-tailPositions cmds = 
-    let addpoints hPoints (tx,ty) prev = foldl (\((hx,hy),(tx,ty),prev) (nhx,nhy) -> let (ntx,nty) = moveTail (tx,ty) (nhx,nhy)
-                                                                                     in ((nhx,nhy), (ntx,nty), S.insert (ntx,nty) prev)) (head hPoints, (tx,ty), prev) (tail hPoints) in
-    let next ((hx,hy), pTail, prev) cmd = case words cmd of
-                                            ["R",n] -> addpoints (map (\x -> (x,hy)) [hx..hx+read n]) pTail prev
-                                            ["L",n] -> addpoints (map (\x -> (x,hy)) [hx, hx-1..hx-read n]) pTail prev
-                                            ["D",n] -> addpoints (map (\y -> (hx,y)) [hy..hy+read n]) pTail prev
-                                            ["U",n] -> addpoints (map (\y -> (hx,y)) [hy, hy-1..hy-read n]) pTail prev in
-    let (_,_,r) = foldl next ((0,0), (0,0), S.empty) cmds
-    in r
+moveTail hPoint tailPoints  = tail $ foldl (\tp np -> let nnp = movePoint np (last tp) in tp ++ [nnp]) [hPoint] tailPoints
 
+addpoints hPoints tailPoints prevPoints = 
+    foldl (\(_,tPoints,prev) (nhx,nhy) -> let newTail = moveTail (nhx,nhy) tPoints 
+                                          in ((nhx,nhy), newTail, S.insert (last newTail) prev)
+          ) (head hPoints, tailPoints, prevPoints) hPoints
+
+tailPositions cmds nTail =
+    let next ((hx,hy), tailPoints, prev) cmd = case words cmd of
+                                                    ["R",n] -> addpoints (map (\x -> (x,hy)) [hx+1..hx+read n]) tailPoints prev
+                                                    ["L",n] -> addpoints (map (\x -> (x,hy)) [hx-1, hx-2..hx-read n]) tailPoints prev
+                                                    ["D",n] -> addpoints (map (\y -> (hx,y)) [hy+1..hy+read n]) tailPoints prev
+                                                    ["U",n] -> addpoints (map (\y -> (hx,y)) [hy-1, hy-2..hy-read n]) tailPoints prev in
+    let (_,_,r) = foldl next ((0,0), replicate nTail (0,0), S.empty) cmds
+    in r
 
 run :: IO ()
 run = do
     content <- readFile "src/day9_input.txt"
     let l = (lines content)
-    print ("puzzle 1: " ++ (show ( length (tailPositions l))))
+    print ("puzzle 1: " ++ (show (length (tailPositions l 1))))
+    print ("puzzle 2: " ++ (show (length (tailPositions l 9))))
--- test/Day9Spec.hs
+++ test/Day9Spec.hs
@@ -14,28 +14,60 @@ ex1 =  ["R 4",
         "L 5",
         "R 2"]
 
+ex2 = [ "R 5",
+        "U 8",
+        "L 8",
+        "D 3",
+        "R 17",
+        "D 10",
+        "L 25",
+        "U 20"]
+
 spec :: Spec
 spec = do
+    describe "movePoint" $ do
+        it "does not move the point in the tail 1" $ do
+            (movePoint (0,0) (1,0)) `shouldBe` (0,0)
+        it "does not move the point in the tail 2" $ do
+            (movePoint (1,0) (0,1)) `shouldBe` (1,0)
+        it "does not move the point in the tail 3" $ do
+            (movePoint (0,0) (0,0)) `shouldBe` (0,0)
+        it "moves the point in tail 1" $ do
+            (movePoint (0,0) (2,0)) `shouldBe` (1,0)
+        it "moves the point in the tail 2" $ do
+            (movePoint (0,0) (0,2)) `shouldBe` (0,1)
+        it "moves the point in the tail 3" $ do
+            (movePoint (0,0) (1,2)) `shouldBe` (1,1)
+        it "moves the point in the tail 4" $ do
+            (movePoint (0,0) (2,1)) `shouldBe` (1,1)
+
     describe "moveTail" $ do
-        it "does not move the tail 1" $ do
-            (moveTail (0,0) (1,0)) `shouldBe` (0,0)
-        it "does not move the tail 2" $ do
-            (moveTail (1,0) (0,1)) `shouldBe` (1,0)
-        it "does not move the tail 3" $ do
-            (moveTail (0,0) (0,0)) `shouldBe` (0,0)
+        it "doesn't move the tail 1" $ do
+            (moveTail (0,0) [(0,0),(0,0)]) `shouldBe` [(0,0),(0,0)]
+        it "doesn't move the tail 2" $ do
+            (moveTail (1,0) [(0,0),(0,0)]) `shouldBe` [(0,0),(0,0)]
         it "moves the tail 1" $ do
-            (moveTail (0,0) (2,0)) `shouldBe` (1,0)
+            (moveTail (2,0) [(0,0),(0,0)]) `shouldBe` [(1,0),(0,0)]
         it "moves the tail 2" $ do
-            (moveTail (0,0) (0,2)) `shouldBe` (0,1)
+            (moveTail (3,0) [(1,0),(0,0)]) `shouldBe` [(2,0),(1,0)]
         it "moves the tail 3" $ do
-            (moveTail (0,0) (1,2)) `shouldBe` (1,1)
+            (moveTail (3,1) [(2,0),(1,0)]) `shouldBe` [(2,0),(1,0)]
         it "moves the tail 4" $ do
-            (moveTail (0,0) (2,1)) `shouldBe` (1,1)
+            (moveTail (3,2) [(2,0),(1,0)]) `shouldBe` [(3,1),(2,1)]
+
+    describe "addpoints" $ do
+        it "finds more visited points" $ do
+            (addpoints [(1,0),(2,0),(3,0),(4,0)] [(0,0),(0,0),(0,0)] S.empty) `shouldBe` ((4,0),[(3,0),(2,0),(1,0)], S.fromList [(0,0),(1,0)])
+            (addpoints [(4,1),(4,2),(4,3),(4,4)] [(3,0),(2,0),(1,0)] (S.fromList [(0,0),(1,0)])) `shouldBe` ((4,4),[(4,3),(4,2),(3,2)], S.fromList [(0,0),(1,0),(2,1),(3,2)])
+
 
     describe "tailPositions" $ do
-        it "find the number of positions where the tail went" $ do
-            (tailPositions ex1) `shouldBe` S.fromList [               (2,-4),(3,-4),
-                                                                             (3,-3),(4,-3),
-                                                               (1,-2),(2,-2),(3,-2),(4,-2),
-                                                                                    (4,-1),
-                                                        (0, 0),(1, 0),(2, 0),(3, 0)]
+        it "find the positions where the tail went in example 1" $ do
+            (tailPositions ex1 1) `shouldBe` S.fromList [               (2,-4),(3,-4),
+                                                                               (3,-3),(4,-3),
+                                                                 (1,-2),(2,-2),(3,-2),(4,-2),
+                                                                                      (4,-1),
+                                                          (0, 0),(1, 0),(2, 0),(3, 0)]
+
+        it "find the number of positions where the tail went in example 2" $ do
+            (length $ tailPositions ex2 9) `shouldBe` 36
