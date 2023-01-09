1beea6c636430520c39bd41e73b82ba9a2e10387
benoitpas <bpasquereau@gmail.com>
2023-01-09 13:23:46 +0000

    Day 14 added addRocks

--- src/Day14.hs
+++ src/Day14.hs
@@ -1,13 +1,18 @@
 module Day14
     (   
         parseLines,
+        addRocks,
         run,
     ) where
+
+import qualified Data.Map as M
 
 import qualified Text.Parsec as P
 import qualified Text.Parsec.Token as T
 import Text.Parsec.Language
 import Data.Functor.Identity
+
+type Point = (Integer, Integer)
 
 lexer :: T.GenTokenParser String u Identity
 lexer = T.makeTokenParser emptyDef
@@ -15,23 +20,37 @@ lexer = T.makeTokenParser emptyDef
 integer :: P.ParsecT String u Data.Functor.Identity.Identity Integer
 integer = T.integer lexer
 
-pairParser :: P.ParsecT String u Data.Functor.Identity.Identity (Integer, Integer)
+pairParser :: P.ParsecT String u Data.Functor.Identity.Identity Point
 pairParser = do
                 i1 <- integer
                 _ <- P.oneOf ","
                 i2 <- integer
                 return (i1,i2)
 
-lineParser :: P.ParsecT String u Data.Functor.Identity.Identity [(Integer, Integer)]
+lineParser :: P.ParsecT String u Data.Functor.Identity.Identity [Point]
 lineParser = pairParser `P.sepBy1` (P.string "-> ")
 
-parseLine :: String -> [(Integer, Integer)]
+parseLine :: String -> [Point]
 parseLine l = case P.parse lineParser "" l of
                 Left err -> error ("parse error at " ++ (show err))
                 Right val -> val
 
-parseLines :: [String] -> [[(Integer, Integer)]]
+parseLines :: [String] -> [[Point]]
 parseLines = map parseLine
+
+intSeq beg end
+    | beg <= end = [beg .. end]
+    | True = [beg,beg-1 .. end]
+
+generatePoints (x1,y1) (x2,y2)
+    | x1 == x2 = zip (repeat x1) (intSeq y1 y2)
+    | True = zip (intSeq x1 x2) (repeat y1)
+
+addRocks :: M.Map Point Char -> [Point] -> M.Map Point Char
+addRocks m rocks = 
+    let (_,r) = foldl (\(p',m') p -> foldl (\(_,m'') np -> (np,M.insert np '#' m'')) (p,m') (generatePoints p p')) 
+                      ((head rocks),m) (tail rocks)
+    in r
 
 run :: IO ()
 run = do 
--- test/Day14Spec.hs
+++ test/Day14Spec.hs
@@ -3,6 +3,8 @@ module Day14Spec (spec) where
 import Test.Hspec
 import Day14
 
+import qualified Data.Map as M
+
 ex1 = [ "498,4 -> 498,6 -> 496,6",
         "503,4 -> 502,4 -> 502,9 -> 494,9"]
 spec :: Spec 
@@ -11,3 +13,7 @@ spec = do
         it "parses the lines" $ do
             (parseLines  ex1) `shouldBe` [[(498,4), (498,6), (496,6)],
                                           [(503,4), (502,4), (502,9), (494,9)]]
+
+    describe "addRocks" $ do
+        it "add the rocks corresponding to a line to the terrain" $ do
+            (addRocks M.empty [(498,4), (498,6), (496,6)]) `shouldBe` (M.fromList [((496,6),'#'),((497,6),'#'),((498,4),'#'),((498,5),'#'),((498,6),'#')])
